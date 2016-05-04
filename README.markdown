c621f0346b6a9224c2bcace9e5d3f0ab8cd6de1e
Sherif Soliman <sherif@ssoliman.com>
2016-05-04 15:05:49 -0400

    Makes caplog able to show tail of log when used with no arguments.

--- README.markdown
+++ README.markdown
@@ -7,10 +7,34 @@ My computer's OS has logging, why don't I?
 `caplog` is a command-line script that I use to write one-line logs about anything I think is worth logging.
 The script adds them to a text file with a timestamp.
 
-Example:
+`caplog` is compatible with both `python2` and `python3`.
+
+Example log file:
 
 ```text
 1462166256,Fixed the variance problem in simple-statistics. Feels great to write code and work on this again.
 1462209509,Its time to move caplog to a place where I can use it everywhere.
 1462209520,And I just pushed a change to simple-statistics. Wanna keep going.
+```
+
+Create an alias to make things easier.
+
+```bash
+alias caplog='python3 /path/to/caplog.py'
+```
+
+Make a new log entry.
+
+```bash
+$ caplog Made progress on my caplog script.
+```
+
+If used without arguments, `caplog` shows the last three entries in the log.
+
+```text
+$ caplog
+
+🚩  May 02 2016 17:18  Its time to move caplog to a place where I can use it everywhere.
+🚩  May 02 2016 17:18  And I just pushed a change to simple-statistics. Wanna keep going.
+🚩  May 03 2016 21:03  Made progress on my caplog script.
 ```
--- caplog.py
+++ caplog.py
@@ -1,20 +1,40 @@
+# -*- coding: utf-8 -*-
+
 from datetime import datetime
+import subprocess
 import sys
 import time
 
-nowtime = int(time.mktime(time.localtime()))
+def from_unix_to_readable(unix_timestamp):
+    return(datetime.fromtimestamp(unix_timestamp).strftime('%B %d %Y %H:%M'))
+
+def add_log_message(logmessage):
+    with open('cap.log', 'a') as logfile:
+       logfile.write(logmessage) 
+       logfile.write('\n')
+
+    logfile.close()
+
+# reference: http://stackoverflow.com/a/136280
+def show_log_tail(n = 3):
+    loglines = subprocess.check_output(["tail", "-n", str(n), "cap.log"])
+    loglines = loglines.decode('utf-8').split('\n')
+    for line in loglines:
+        if line != '':
+            logdate, logmsg = line.split(',')
+            logdate = from_unix_to_readable(float(logdate))
+            formatted_log = u'🚩 ' + '  ' + logdate + '  ' + logmsg
+            print(formatted_log)
+
 arguments = sys.argv
 
-if len(arguments) > 1: arguments.pop(0)
+if len(arguments) > 1:
+    nowtime = int(time.mktime(time.localtime()))
+    arguments.pop(0)
+    status = ' '.join(arguments)
+    logmessage = str(nowtime) + ',' + status
+    add_log_message(logmessage)
+else:
+    show_log_tail()
 
-status = ' '.join(arguments)
-logmessage = str(nowtime) + ',' + status
 
-def from_unix_to_readable(unix_timestamp):
-    return(datetime.fromtimestamp(unix_timestamp).strftime('%B %d, %Y %H:%M:%S'))
-
-with open('cap.log', 'a') as logfile:
-   logfile.write(logmessage) 
-   logfile.write('\n')
-
-logfile.close()
