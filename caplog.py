f597847c2fc67847d859e52b6d429b048ce693ef
Sherif Soliman <sherif@ssoliman.com>
2016-05-08 20:24:13 -0400

    Fixes path to cap.log file and makes caplog accesible from any path.

--- caplog.py
+++ caplog.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from os.path import expanduser
 import subprocess
 import sys
 import time
+
+# reference: http://stackoverflow.com/a/4028943
+home = expanduser("~")
+log_file_path = home + '/cap.log'
 
 def from_unix_to_readable(unix_timestamp):
     return(datetime.fromtimestamp(unix_timestamp).strftime('%B %d %Y %H:%M'))
 
 def add_log_message(logmessage):
-    with open('cap.log', 'a') as logfile:
+    with open(log_file_path) as logfile:
        logfile.write(logmessage) 
        logfile.write('\n')
 
@@ -17,7 +22,7 @@ def add_log_message(logmessage):
 
 # reference: http://stackoverflow.com/a/136280
 def show_log_tail(n = 3):
-    loglines = subprocess.check_output(["tail", "-n", str(n), "cap.log"])
+    loglines = subprocess.check_output(["tail", "-n", str(n), log_file_path])
     loglines = loglines.decode('utf-8').split('\n')
     for line in loglines:
         if line != '':
