e6ef983eabac87f5fb53b3586add73074e450c44
Sherif Soliman <sherif@ssoliman.com>
2016-05-18 15:00:14 -0400

    Adds show_random_log() function and factors out json->log formatting func.

--- caplog.py
+++ caplog.py
@@ -4,12 +4,19 @@ from datetime import datetime
 from os.path import expanduser
 import json
 import subprocess
+import random
 import sys
 import time
 
 # reference: http://stackoverflow.com/a/4028943
 home = expanduser("~")
 log_file_path = home + '/cap.log'
+
+def format_log_entry(jsonentry):
+    logdate = from_unix_to_readable(jsonentry['timestamp'])
+    formatted_entry = u'🚩 ' + '  ' + logdate + '  ' + jsonentry['entry']
+    return(formatted_entry)
+
 
 def from_unix_to_readable(unix_timestamp):
     return(datetime.fromtimestamp(unix_timestamp).strftime('%B %d %Y %H:%M'))
@@ -36,9 +43,12 @@ def show_log_tail(n = 3):
     entries = read_all_entries(log_file_path)
 
     for entry in entries[n*-1:]:
-        logdate = from_unix_to_readable(entry['timestamp'])
-        formatted_entry = u'🚩 ' + '  ' + logdate + '  ' + entry['entry']
-        print(formatted_entry)
+        print(format_log_entry(entry))
+
+def show_random_log():
+    entries = read_all_entries(log_file_path)
+
+    print(format_log_entry(random.choice(entries)))
 
 if __name__ == "__main__":
     arguments = sys.argv
